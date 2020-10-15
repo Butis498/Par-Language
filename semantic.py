be73560458400003aa01b6e37d49ff25d47ab7f6
Esteban <estebansalazar498@gmail.com>
2020-10-15 00:44:58 -0500

    add semantic cube

--- semantic.py
+++ semantic.py
@@ -1,137 +1,132 @@
 
-semantic = {
-  'int': {
-    '+' : {
-        'int':'int',
-        'float':'float'
-    },
-    '-':{
-        'int':'int',
-        'float':'float'
-    },
-    '=':{
-        'int':'int',
-        'float':'float'
-    },
-    '*':{
-        'int':'int',
-        'float':'float'
-    },
-    '/':{
-        'int':'int',
-        'float':'float'
-    }
-    '.':{
-        'int':'int',
-        'float':'float'
-    },
-    '<':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '>':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '<=':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '>=':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '==':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '!=':{
-        'int':'bool',
-        'float':'bool'
-    }
-}
+class Semantic():
+    def __init__(self):
 
- 'float': {
-    '+' : {
-        'int':'float',
-        'float':'float'
-    },
-    '-':{
-        'int':'float',
-        'float':'float'
-    },
-    '=':{
-        'int':'float',
-        'float':'float'
-    },
-    '*':{
-        'int':'float',
-        'float':'float'
-    },
-    '/':{
-        'int':'float',
-        'float':'float'
-    }
-    '.':{
-        'int':'float',
-        'float':'float'
-    },
-    '<':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '>':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '<=':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '>=':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '==':{
-        'int':'bool',
-        'float':'bool'
-    },
-    '!=':{
-        'int':'bool',
-        'float':'bool'
-    }
-}
+        self.semantic_cube = {
+            'int': {
+                '+': {
+                    'int': 'int',
+                    'float': 'float'
+                },
+                '-': {
+                    'int': 'int',
+                    'float': 'float'
+                },
+                '=': {
+                    'int': 'int',
+                    'float': 'int'
+                },
+                '*': {
+                    'int': 'int',
+                    'float': 'float'
+                },
+                '/': {
+                    'int': 'int',
+                    'float': 'float'
+                },
+    
+                '<': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '>': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '<=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '>=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '==': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '!=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                }
+            },
 
- 'bool': {
-    '=' : {
-        'bool':'bool'
-    },
-    '==':{
-        'bool':'bool'
-    },
-    '!=':{
-        'bool':'bool'
-    }
-    '&':{
-        'bool':'bool'
-    }
-    '|':{
-        'bool':'bool'
-    }
- }
+            'float': {
+                '+': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '-': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '=': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '*': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                '/': {
+                    'int': 'float',
+                    'float': 'float'
+                },
+                
+                '<': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '>': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '<=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '>=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '==': {
+                    'int': 'bool',
+                    'float': 'bool'
+                },
+                '!=': {
+                    'int': 'bool',
+                    'float': 'bool'
+                }
+            },
 
- 'char': {
-    '=' : {
-        'char':'char'
-    },
-    '==':{
-        'char':'bool'
-    },
-    '!=':{
-        'char':'bool'
-    }
- }
+            'bool': {
+                '=': {
+                    'bool': 'bool'
+                },
+                '==': {
+                    'bool': 'bool'
+                },
+                '!=': {
+                    'bool': 'bool'
+                },
+                '&': {
+                    'bool': 'bool'
+                },
+                '|': {
+                    'bool': 'bool'
+                }
+            },
 
- 
-   
-}
+            'char': {
+                '=': {
+                    'char': 'char'
+                },
+                '==': {
+                    'char': 'bool'
+                },
+                '!=': {
+                    'char': 'bool'
+                }
+            }
+
+        }
