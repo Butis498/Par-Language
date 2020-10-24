b8e38bbae64f9f1b973b34bbebed7ddf4ba5b648
Esteban <estebansalazar498@gmail.com>
2020-10-23 23:34:00 -0500

    add variables and func tables

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 Esteban Salazar
 Adrian Gonzalez
 
-El proyecto ya tiene definido tanto el lexico como la gramatica, cuenta con numeros , Ids , y todos los tokens necesarios
-Se implemento en modo de clases para facilitar futuras partes del proyecto.
-Se creo el cubo semantico para la funcionalidad del compilador.
+En este avanze se implemto las tablas de variables y funciones incluyendo el uso de contadores para la memoria base de cada tipo de variable
--- semantic.py
+++ semantic.py
@@ -128,5 +128,67 @@ class Semantic():
                     'char': 'bool'
                 }
             }
-
         }
+        self.MEMORY_SPACE = 1000
+        self.next_memory_block = self.MEMORY_SPACE
+        self.variables_table = {}
+        self.functions_table = {}
+        self.jumps_stack = []
+        
+        self.variables_base_memory = {
+
+            'global':{'int':self.asign_memory_base(),
+                    'float':self.asign_memory_base(),
+                    'char':self.asign_memory_base()},
+
+            'local':{'int':self.asign_memory_base(),
+                    'float':self.asign_memory_base(),
+                    'char':self.asign_memory_base()},
+
+            'temp':{'int':self.asign_memory_base(),
+                    'float':self.asign_memory_base(),
+                    'char':self.asign_memory_base()},
+
+            'const':{'int':self.asign_memory_base(),
+                    'float':self.asign_memory_base(),
+                    'char':self.asign_memory_base()}
+            }
+
+    def asign_memory_base(self):
+
+        self.next_memory_block += self.MEMORY_SPACE
+        return self.next_memory_block
+
+    def remove_variable(self,variable_name):
+
+        if variable_name not in self.variables_table.keys():
+            raise KeyError("Variable "+ variable_name  +" already exists")
+        
+        del self.variables_table[variable_name]
+
+    def insert_variable(self,variable_name,variable_type,socope):
+
+        if variable_name in self.variables_table.keys():
+
+            raise KeyError("Variable "+ variable_name  +" already exists")
+        
+        variable = {variable_name:{'type': variable_type,'memory_addr':self.variables_base_memory[socope][variable_type]}}
+        self.variables_table.update(variable)
+        self.variables_base_memory[socope][variable_type] += 1
+
+    def insert_function(self,function_name,function_type):
+
+        if function_name in self.functions_table.keys():
+
+            raise KeyError("Function "+ function_name +" already exists")
+        
+        function = {function_name:{'type': function_type}}
+        self.functions_table.update(function) 
+
+    def function_call(self,function_name):
+
+        if function_name not in self.functions_table.keys():
+
+            raise KeyError("Function " + str(function_name) +" not declared")
+
+
