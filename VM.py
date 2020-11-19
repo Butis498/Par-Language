1a92db74103337723f13b999366aab00a60d74b5
Adrian Gonzalez <you@example.com>
2020-11-18 21:51:55 -0600

    Switch de virtual machine

--- VM.py
+++ VM.py
@@ -3,4 +3,67 @@ class VirtualMachine():
     def __init__(self):
         pass
 
-        
+        if(operation == "*") {
+
+        } elif (operation == "+"){
+
+        } elif (operation == "-"){
+
+        }elif (operation == "/"){
+
+        }elif (operation == "<"){
+
+        }elif (operation == ">"){
+
+        }elif (operation == ">="){
+
+        }elif (operation == "<="){
+
+        }elif (operation == "="){
+
+        }elif (operation == "=="){
+
+        }elif (operation == "goto"){
+
+        }elif (operation == "gotof"){
+
+        }elif (operation == "gosub"){
+
+        }elif (operation == "param"){
+
+        }elif (operation == "ver"){
+
+        }elif (operation == "ver_dim"){
+
+        }elif (operation == "era"){
+
+        }elif (operation == "end"){
+
+        }elif (operation == "endfunc"){
+
+        }elif (operation == "write"){
+
+        }elif (operation == "read"){
+
+        }elif (operation == "+mat"){
+
+        }elif (operation == "-mat"){
+
+        }elif (operation == "*mat"){
+
+        }elif (operation == "=mat"){
+
+        }elif (operation == "-arr"){
+
+        }elif (operation == "+arr"){
+
+        }elif (operation == "*arr"){
+
+        }elif (operation == "=arr"){
+
+        }elif (operation == "transpose"){
+
+        }elif (operation == "inverse"){
+
+        }
+
